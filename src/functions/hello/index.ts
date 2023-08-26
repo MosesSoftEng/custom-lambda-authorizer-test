e401fde14ecbac52298624ef9269cf099fca16ad
Moses Ngige <moses.soft.eng@gmail.com>
2023-08-26 23:59:47 +0300

    Add authorizer property to hello function

--- src/functions/hello/index.ts
+++ src/functions/hello/index.ts
@@ -1,19 +1,23 @@
-import schema from './schema';
-import { handlerPath } from '@libs/handler-resolver';
+import schema from "./schema";
+import { handlerPath } from "@libs/handler-resolver";
 
 export default {
-  handler: `${handlerPath(__dirname)}/handler.main`,
-  events: [
-    {
-      http: {
-        method: 'post',
-        path: 'hello',
-        request: {
-          schemas: {
-            'application/json': schema,
-          },
-        },
-      },
-    },
-  ],
+	handler: `${handlerPath(__dirname)}/handler.main`,
+	events: [
+		{
+			http: {
+				method: "post",
+				path: "hello",
+				request: {
+					schemas: {
+						"application/json": schema,
+					},
+				},
+				// Add authorizer property with name of custom authorizer function.
+				authorizer: {
+					name: 'auth',
+				}
+			},
+		},
+	],
 };
