f62eede61d8904574b8ca3f3f216b010f542d2b7
Moses Ngige <moses.soft.eng@gmail.com>
2023-08-26 23:57:27 +0300

    Define custom auth function definitions

--- serverless.ts
+++ serverless.ts
@@ -1,56 +1,42 @@
-import type {AWS} from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
+import hello from "@functions/hello";
 
 
 const serverlessConfiguration: AWS = {
-	service: 'custom-authorization',
-	frameworkVersion: '3',
-	plugins: ['serverless-esbuild'],
+	service: "custom-authorization",
+	frameworkVersion: "3",
+	plugins: ["serverless-esbuild"],
 	provider: {
-		name: 'aws',
-		runtime: 'nodejs14.x',
+		name: "aws",
+		runtime: "nodejs14.x",
 		apiGateway: {
 			minimumCompressionSize: 1024,
 			shouldStartNameWithService: true,
 		},
 		environment: {
-			AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-			NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+			AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+			NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
 		},
 	},
-	// import the function via paths
+	// Lambda function definitions.
 	functions: {
+		// Custom token authorizer lambda function definition.
 		auth: {
-			handler: 'src/functions/auth.auth',
+			handler: "src/functions/auth.auth",
 		},
-		hello: {
-			handler: 'src/functions/hello/handler.main',
-			events: [
-				{
-					http: {
-						method: 'get',
-						path: 'hello',
-						authorizer: {
-							name: 'MyCustomAuthorizer',
-							arn: 'arn:aws:lambda:us-east-1:137627469964:function:custom-authorization-dev-auth',
-							resultTtlInSeconds: 300,
-							identitySource: 'method.request.header.Authorization',
-							type: 'request',
-						},
-					},
-				},
-			],
-		}
+		hello,
 	},
-	package: {individually: true},
+
+	package: { individually: true },
 	custom: {
 		esbuild: {
 			bundle: true,
 			minify: false,
 			sourcemap: true,
-			exclude: ['aws-sdk'],
-			target: 'node14',
-			define: {'require.resolve': undefined},
-			platform: 'node',
+			exclude: ["aws-sdk"],
+			target: "node14",
+			define: { "require.resolve": undefined },
+			platform: "node",
 			concurrency: 10,
 		},
 	},
